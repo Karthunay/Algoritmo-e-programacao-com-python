6f46795a23b4005083a681b32efe66de5a227ab2
Kellen Karthunay <83416962+Karthunay@users.noreply.github.com>
2021-08-07 21:31:16 -0300

    Delete aula 1.py

--- aula 1.py
+++
@@ -1,8 +0,0 @@
-#Realize o cadastro de um aluno, com as seguintes informações: Nome, endereço, cidade e idade
-
-nome = str(input('Digite o seu nome: '))
-endereco = str(input('Digite o seu endereço: '))
-cidade  = str(input('Digite a sua cidade: '))
-idade = int(input('Digite a sua idade: '))
-
-print(f'{nome}, tem {idade}anos e reside em {endereco}, na cidade de {cidade}. ')
